00a2159cf21420f04bbc7ad6daf5e5945a6ccf9d
dnzlthompson1 <dnzlthompson1@yahoo.com>
2015-11-02 17:41:32 -0500

    test

--- LargeNumber.java
+++ LargeNumber.java
@@ -9,11 +9,17 @@ public class LargeNumber
 	{
 		int[]num={-56,33,71,-6,56,7};
 		int largenum=num[0];
-		int i=0;
-		while (i<num.length)
+		int i=1;
+		if(num!=null)
 		{
-			if(largenum<num[i]){largenum=num[i];}
-			i++;
+			if(num.length>0)
+			{
+				while (i<num.length)
+				{
+					if(largenum<num[i]){largenum=num[i];}
+					i++;
+				}
+			}
 		}
 		return largenum;
 	}
@@ -21,10 +27,15 @@ public class LargeNumber
 	{
 		int[]num={-56,33,76,56,7};
 		int largenum=num[0];
-		
-		for (int i=0;i<num.length;i++)
+		if(num!=null)
 		{
-			if(largenum<num[i]){largenum=num[i];}
+			if(num.length>0)
+			{
+				for (int i=1;i<num.length;i++)
+				{
+					if(largenum<num[i]){largenum=num[i];}
+				}
+			}
 		}
 		return largenum;
 	}
